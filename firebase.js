600e08e92f35dfe3c09a6a7e7277fb8b5fcac06f
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-03-09 23:12:47 +0400

    update

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCr8X2oH35jM9vh04wCH2TVPlv5zxN0YGw",
+    authDomain: "netflix-react-db708.firebaseapp.com",
+    projectId: "netflix-react-db708",
+    storageBucket: "netflix-react-db708.appspot.com",
+    messagingSenderId: "1053325546726",
+    appId: "1:1053325546726:web:efa23b69c57fc05a3e5727"
+  };
---
+++ timer.js
@@ -0,0 +1,6 @@
+/*
+2/5
+2:47/1:36:09
+https://youtu.be/CLMo0W7mTVo
+
+*/
