d279fbfcdaee5ab467d126b2508430c0b878ddc5
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-03-11 21:40:39 +0400

    update

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import './Login.css';
+import SignUpScreen from './SignUpScreen';
 
 function Login() {
 
@@ -23,23 +24,31 @@ function Login() {
             </div>
 
             <div className="login__body">
-                <>
-                    <h1>Unlimited movies, TV shows, and more.</h1>
-                    <h2>Watch anywhere. Cancel anytime.</h2>
-                    <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
-                    <div className="login__input">
-                        <form>
-                            <input 
-                                type="email" 
-                                placeholder="Email address" />
-                            <button 
-                                onClick={ () => setSignIn(true)}
-                                className="login__tryButton">
-                                Try 30 Days Free 
-                            </button>
-                        </form>
-                    </div>
-                </>
+                {
+                    signIn ? (
+                        <SignUpScreen/>
+                    ) : (
+                        <>
+                            <h1>Unlimited movies, TV shows, and more.</h1>
+                            <h2>Watch anywhere. Cancel anytime.</h2>
+                            <h3>Ready to watch? Enter your email to create or restart your membership.</h3>
+                            <div className="login__input">
+                                <form>
+                                    <input 
+                                        type="email" 
+                                        placeholder="Email address" />
+                                    <button 
+                                        onClick={ () => setSignIn(true)}
+                                        className="login__tryButton">
+                                        Try 30 Days Free 
+                                    </button>
+                                </form>
+                            </div>
+                        </>
+
+                    )
+                    }
+                
             </div>
         </div>
     )
