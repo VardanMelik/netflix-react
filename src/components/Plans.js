3f7bc90a7f1bd9e72a74c15350344e8574e85f1a
EYE MOTION <eyemotiongamestudio@gmail.com>
2021-03-15 19:55:35 +0400

    update

--- src/components/Plans.js
+++ src/components/Plans.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
-import './Plans';
+import './Plans.css';
 import db from '../firebase';
+import { useSelector } from 'react-redux';
+import { loadStripe } from '@stripe/stripe-js'; 
 
 function Plans() {
     const [products, setProducts] = useState([]);
+    const user = useSelector(selectUser);
 
     useEffect( () => {
         db.collection('products')
         .where('active', '==', true )
         .get().then(querySnapshot => {
-            const produts = {};
+            const products = {};
 
             querySnapshot.forEach( async productDoc => {
                 products[productDoc.id] = productDoc.data();
@@ -27,6 +30,34 @@ function Plans() {
         });
     }, [])
 
+    const loadCheckout = async (priceId) => {
+        //console.log('ID: ',priceId);
+        const docRef = await db
+            .collection('customers')
+            .doc(user.uid)
+            .collection('checkout_sessions')
+            .add({
+                price: priceId,
+                success_url: window.location.origin,
+                cancel_url: window.location.origin
+            });
+
+            docRef.onSnapshot( async (snap) => {
+                const { error, sessionId} = snap.data();
+
+                if (error) {
+                    alert(`An error occured: ${error.message}`);
+                }
+
+                if (sessionId) {
+
+                    // Init Stripe
+                    const stripe = await loadStripe()
+                }
+            })
+
+    }
+
     console.log('Products: ',products);
 
     return (
@@ -36,12 +67,15 @@ function Plans() {
                     // TODO add some logic to check if the users 
                     // subscription is active
                     return (
-                        <div className="plans__plan">
+                        <div key={productId} className="plans__plan">
                             <div className="plans__info">
                                 <h5>{productData.name}</h5>
                                 <h6>{productData.description}</h6>
                             </div>
-                            <button>
+                            <button 
+                                onClick={ () => loadCheckout(
+                                    productData.prices.priceId
+                                )}>
                                 Subscribe
                             </button>
                         </div>
--- timer.js
+++ timer.js
@@ -1,6 +1,6 @@
 /*
 4/5
-01:23:05/2:15:09
+01:29:05/2:15:09
 https://www.youtube.com/watch?v=AHYVxg-lJX4&ab_channel=SonnySangha
 
 */
